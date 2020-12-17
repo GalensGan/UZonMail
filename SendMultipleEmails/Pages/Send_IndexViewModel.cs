be4c29ba02c1045602a14fb34430c09a5c48fb25
galensgan <gmx_galens@163.com>
2020-12-17 08:17:53 +0800

    update

---
+++ SendMultipleEmails/Pages/Send_IndexViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SendMultipleEmails.Datas;
+using SendMultipleEmails.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMultipleEmails.Pages
+{
+    class Send_IndexViewModel:ScreenChild
+    {
+        public Send_IndexViewModel(Store store) : base(store) { }
+
+
+        public string NewTip { get; set; } = "新建发送";
+
+        public void New()
+        {
+            InvokeTo(new GalensSDK.StyletEx.InvokeParameter() { InvokeId = InvokeID.Send_New.ToString() });
+        }
+
+        public void History()
+        {
+            InvokeTo(new GalensSDK.StyletEx.InvokeParameter() { InvokeId = InvokeID.Send_Sent.ToString() });
+        }
+    }
+}
