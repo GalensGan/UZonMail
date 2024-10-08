4b37de3c1219d167f52d6ebfe47944694df32cfc
galens.gmx <260827400@qq.com>
2024-10-08 08:49:26 +0800

    feat: 重构缓存功能

--- backend-src/UzonMailDB/SQL/Unsubscribes/UnsubscribeEmail.cs
+++ backend-src/UzonMailDB/SQL/Unsubscribes/UnsubscribeEmail.cs
@@ -8,6 +8,12 @@ namespace UZonMail.DB.SQL.Unsubscribes
     public class UnsubscribeEmail : OrgId
     {
         /// <summary>
+        /// 退订时的 IP 地址
+        /// 方便定位退订的来源
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
         /// 退订邮箱
         /// </summary>
         public string Email { get; set; }
